0ea06b6cad6f8187fcaa0aeca8a6a99527ee5572
Zhang Yuntong <ang.unong@gmail.com>
2020-07-14 00:37:12 +0800

    rebuilding site Tue Jul 14 00:37:08 +08 2020

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ fi
 echo -e "\n\033[0;32mBuilding the new site...\033[0m"
 hugo -D
 
-echo -e "\n\n\033[0;32mCommiting to the static files folder...\033[0m"
+echo -e "\n\n\033[0;32mCommitting to the static files folder...\033[0m"
 cd public
 git add .
 git commit -m "$msg"
@@ -16,7 +16,7 @@ git commit -m "$msg"
 echo -e "\n\n\033[0;32mUpdating static files remote...\033[0m"
 git push origin master
 
-echo -e "\n\n\033[0;32mCommiting to the source files repo...\033[0m"
+echo -e "\n\n\033[0;32mCommitting to the source files repo...\033[0m"
 cd ..
 git add .
 git commit -m "$msg"
